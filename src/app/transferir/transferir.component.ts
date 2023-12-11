97ba3d47e49551b9aeff458aa103e1c11cbff6a9
João Victor <86169183+KundeVillanova@users.noreply.github.com>
2023-12-10 22:59:19 -0300

    commit da madrug

--- src/app/transferir/transferir.component.ts
+++ src/app/transferir/transferir.component.ts
@@ -4,47 +4,49 @@ import { PilaCoinService } from '../pila-coin.service';
 import { Usuario } from '../models/Usuario';
 
 @Component({
-  selector: 'app-transferir',
-  templateUrl: './transferir.component.html',
-  styleUrls: ['./transferir.component.css']
+    selector: 'app-transferir',
+    templateUrl: './transferir.component.html',
+    styleUrls: ['./transferir.component.css']
 })
 export class TransferirComponent implements OnInit {
-  usuarios: Usuario[] = [];
-  selectedUserId: number | null = null;
+    usuarios: Usuario[] = [];
+    selectedUserId: number | null = null;
 
-  constructor(private pilaCoinService: PilaCoinService) {}
+    constructor(private pilaCoinService: PilaCoinService) {}
 
-  ngOnInit(): void {
-    this.carregarUsuarios();
-  }
-
-  carregarUsuarios(): void {
-    this.pilaCoinService.getUsuarios().subscribe(
-      (data: Usuario[]) => {
-        this.usuarios = data;
-      },
-      error => {
-        console.error('Erro ao carregar usuários:', error);
-      }
-    );
-  }
-
-  onUsuarioSelect(id: number): void {
-    this.selectedUserId = id;
-  }
-
-  transferirPila(): void {
-    if (this.selectedUserId !== null) {
-      this.pilaCoinService.transferirPila(this.selectedUserId).subscribe(
-        (response: string) => {
-          console.log(response); // Adicione tratamento conforme necessário
-          // Após a transferência, você pode atualizar a lista de usuários
-          this.carregarUsuarios();
-        },
-        error => {
-          console.error('Erro ao transferir PilaCoin:', error);
-        }
-      );
+    ngOnInit(): void {
+        this.carregarUsuarios();
     }
-  }
+
+    carregarUsuarios(): void {
+        this.pilaCoinService.getUsuarios().subscribe(
+            (data: Usuario[]) => {
+                this.usuarios = data;
+            },
+            error => {
+                console.error('Erro ao carregar usuários:', error);
+            }
+        );
+    }
+
+    onUsuarioSelect(id: number): void {
+        this.selectedUserId = id;
+    }
+
+    transferirPila(): void {
+        if (this.selectedUserId !== null) {
+            this.pilaCoinService.transferirPila(this.selectedUserId).subscribe(
+                (response: string) => {
+                    console.log(response); 
+                    alert('Transferência de PilaCoin realizada com sucesso!');
+                    this.selectedUserId = null;
+                    this.carregarUsuarios();
+                },
+                error => {
+                    console.error('Erro ao transferir PilaCoin:', error);
+                    alert('Erro ao transferir PilaCoin. Por favor, tente novamente.');
+                }
+            );
+        }
+    }
 }
